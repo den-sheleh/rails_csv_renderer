c701c9859406ee7dc06954505921e393dc3da06d
Dennis Sheleh <den.sheleh@gmail.com>
2014-07-14 16:55:46 +0300

    Fix: init renderer

--- lib/rails_csv_renderer/renderer.rb
+++ lib/rails_csv_renderer/renderer.rb
@@ -3,12 +3,13 @@ require 'action_controller/metal/renderers'
 
 module RailsCsvRenderer
   class Renderer
-    def initialize!
-      ActionController::Renderers.add :csv do |obj, options|
+    def self.initialize!
+      ActionController::Renderers.add(:csv) do |obj, options|
         filename = options[:filename] || "#{ Rails.application.class.parent_name }-report-#{ Time.current }.csv"
-        csv.extend RailsCsvRenderer::Renderable
-        data = csv.to_custom_csv(options)
-        send_data data, type: Mime::CSV, disposition: "attachment; filename=#{filename}"
+        obj.extend RailsCsvRenderer::Renderable
+        data = obj.to_custom_csv(options)
+        send_data(data, type: Mime::CSV, disposition: "attachment; filename=#{filename}")
+      end
     end
   end
 end
